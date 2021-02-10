facfa51d20f727267029861912bf088b4ac74166
odiinthefirst <76893858+odiinthefirst@users.noreply.github.com>
2021-02-10 08:40:10 +0000

    scoreboard done upgrade to sqlite3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const HypixelAPI = require('hypixel-api');
 const SQLite = require("better-sqlite3");
-const sql = new SQLite("./leaderboard.sqlite");
+const sql = new SQLite("./leaderboard.sqlite3");
 const creds = require("./creds.json")
 
 const Hclient = new HypixelAPI(creds.hypixel)
@@ -15,19 +15,19 @@ client.on('ready', () => {
   const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
   if (!table['count(*)']) {
     // If the table isn't there, create it and setup the database correctly.
-    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, points INTEGER, wins INTEGER, losses INTEGER);").run();
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, points INTEGER, games INTEGER, wins INTEGER, losses INTEGER);").run();
     // Ensure that the "id" row is always unique and indexed.
     sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
     sql.pragma("synchronous = 1");
     sql.pragma("journal_mode = wal");
   }
   client.getScore = sql.prepare("SELECT * FROM scores WHERE id = ?");
-  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, points, wins, losses) VALUES (@id, @points, @wins, @losses);");
+  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, points, games, wins, losses) VALUES (@id, @points, @games, @wins, @losses);");
 });
 
 
 client.on('message', msg => {
-  if (msg.content.startsWith('!bw ')) {
+  if (msg.content.startsWith('b!bw ')) {
 
     const playername = msg.content.substr(5)
 
@@ -81,7 +81,7 @@ client.on('message', msg => {
 
     })
   }
-  if (msg.content === '!ping') {
+  if (msg.content === 'b!ping') {
     const exampleEmbed = new Discord.MessageEmbed()
       .setColor('#0000f9')
       .setTitle(`PONG!`)
@@ -92,32 +92,128 @@ client.on('message', msg => {
 
       msg.channel.send(exampleEmbed);
   }
-  if (msg.content.startsWith('!sb getscore ')) {
-    score = client.getScore.get(msg.author.id);
+  if (msg.content.startsWith('b!sb myscore')) {
+
+    try {
+      score = client.getScore.get(msg.author.id);
+      if (!score) {
+        score = {
+          id: msg.author.id,
+          points: 0,
+          games: 0,
+          wins: 0,
+          losses: 0
+        }
+      }
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#00f000')
+      .setTitle(msg.author.username)
+      .addFields(
+        { name: 'Games', value: "`"+score.games+"`", inline: true },
+        { name: 'Points', value: "`"+score.points+"`", inline: true },
+        { name: 'Wins', value: "`"+score.wins+"`", inline: true },
+        { name: 'Losses', value: "`"+score.losses+"`", inline: true },
+      )
+      msg.channel.send(exampleEmbed);
+
+      client.setScore.run(score);
+
+    } catch (error){
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle(String(error))
+      .setDescription("**Make sure you mentioned the user!**\n*If you still get this problem, create a support ticket!*")
+      msg.channel.send(exampleEmbed);
+  
+    }
+  }
+  if (msg.content === "b!sb") {
+    msg.guild.members.fetch()
+    try {
+      const top10 = sql.prepare("SELECT * FROM scores ORDER BY points DESC;").all()
+      const embed = new Discord.MessageEmbed()
+        .setTitle("Leaderboard")
+        .setDescription("Our leaderboard!")
+        .setColor('#0000f9')
+        .setFooter('By Odiin#0001.', 'https://i.imgur.com/gtSR0hn.gif');
+      var i = 1;
+      for(const data of top10) {
+          embed.addField(i+": "+client.users.cache.get(data.id).tag, "Points: `"+data.points+"` Games: `"+data.games+"` Wins: `"+data.wins+"` Losses: `"+data.losses+"`");
+          i++
+        }
+      msg.channel.send(embed)
+    } catch (error){
+      console.log(error)
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle(error)
+      .setDescription("*If you still get this problem, DM Odiin#0001 (<@263962785480704000>)*")
+      msg.channel.send(exampleEmbed);
+    }
+  }
+  if (msg.content.startsWith('b!sb addwin ')) {
+    try {
+      const userID = msg.mentions.users.first().id
+    console.log(userID)
+    score = client.getScore.get(userID);
     if (!score) {
       score = {
-        id: msg.author.id,
-        points: 0,
-        wins: 0,
+        id: userID,
+        points: 1,
+        games: 1,
+        wins: 1,
         losses: 0
       }
+      client.setScore.run(score);
     }
-    msg.reply(score.points)
-    client.setScore.run(score);
+    else {
+      score.wins++;
+      score.points++;
+      score.games++;
+      client.setScore.run(score);
+    }
+    } catch (error){
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle(String(error))
+      .setDescription("**Make sure you mentioned the user!**\n*If you still get this problem, create a support ticket!*")
+      msg.channel.send(exampleEmbed);
+  
+    }
+
   }
-  if (msg.content.startsWith('!sb addwin ')) {
-    score = client.getScore.get(msg.author.id);
-    if (!score) {
-      score = {
-        id: msg.author.id,
-        points: 0,
-        wins: 0,
-        losses: 0
+  if (msg.content.startsWith('b!sb addloss ')) {
+    try {
+      const userID = msg.mentions.users.first().id
+      console.log(userID)
+      score = client.getScore.get(userID);
+      if (!score) {
+        score = {
+          id: userID,
+          points: -1,
+          games: 1,
+          wins: 0,
+          losses: 1
+        }
+        client.setScore.run(score);
       }
+      else {
+        score.losses++;
+        score.points--;
+        score.games++;
+        client.setScore.run(score);
+      }
+    } catch (error){
+      const exampleEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle(String(error))
+      .setDescription("**Make sure you mentioned the user!**\n*If you still get this problem, create a support ticket!*")
+      msg.channel.send(exampleEmbed);
+  
     }
-    msg.reply(score.points)
-    client.setScore.run(score);
+    
+
   }
-  });
+  })
 
 
